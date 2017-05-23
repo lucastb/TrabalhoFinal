097d4342815bfb2a8ace871d5c00c5f6f4978f2f
Victor <victor.cauduro@acad.pucrs.br>
2017-05-23 03:49:39 -0300

    Adicionado letras minusculas no gerador de codigo de barras, pra diminuir a possibilidade de repetição.

--- EstacionamentoWebApp/Controllers/BarCodeGeneratorTM.cs
+++ EstacionamentoWebApp/Controllers/BarCodeGeneratorTM.cs
@@ -10,7 +10,7 @@ namespace EstacionamentoWebApp.Controllers
 
         public string generateCode()
         {
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             var stringChars = new char[11];
             var random = new Random();
             for (int i = 0; i < stringChars.Length; i++)
