42e02b83af97fd2c7be9e4b1460bb7534bdc42fd
Victor <victor.cauduro@acad.pucrs.br>
2017-05-29 04:11:56 -0300

    Teste facade

---
+++ EstacionamentoWebAppTests/BLL/FacadeTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EstacionamentoWebApp.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoWebApp.BLL.Tests
+{
+    [TestClass()]
+    public class FacadeTests
+    {
+        Facade pepe = new Facade();
+        [TestMethod()]
+        public void nDeVagasDisponiveisTest()
+        {
+            Console.WriteLine(pepe.nDeVagasDisponiveis());
+        }
+    }
+}
